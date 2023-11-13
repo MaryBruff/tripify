b3a090044563df8ef960679d9bde6a420962e644
Mary Bruff <marybruff5@gmail.com>
2023-11-12 19:46:35 -0700

    add calculate test file

---
+++ test/calculateTests-test.js
@@ -0,0 +1,4 @@
+// import { expect } from "chai";
+
+// import { destinationsData, travelersData, tripsData } from '../src/Data/sample-test-data';
+// import  { filterUserTrips } from "../src/functions.js"
